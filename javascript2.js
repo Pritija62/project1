8da74044ebc1fc2392ec6face11438f7cdbe4b88
Pritija62 <138745743+Pritija62@users.noreply.github.com>
2023-08-26 22:25:39 -0700

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,13 @@
+function onsubmit(event)
+{
+    event.preventDefault();
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var gender=document.querySelector('input[name="gender"]:checked').value;
+    document.getElementById('formData').innerHTML="Name:"+ name+"<br>Email:"+email +"<br>Gender:"+gender;
+}    
+    window.onload=function()
+    {
+       
+       document.getElementById('contactForm').addEventListener('submit',onsubmit);
+    }
